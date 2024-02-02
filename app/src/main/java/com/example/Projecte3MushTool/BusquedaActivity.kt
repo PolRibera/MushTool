cd0cf732b44fc5d88b75e9f31052d8f6e2fab5ea
polri <ribera.aymerich.pol@alumnat.copernic.cat>
2024-02-02 19:59:23 +0100

    BD Implementation (Temporal)

--- app/src/main/java/com/example/Projecte3MushTool/BusquedaActivity.kt
+++ app/src/main/java/com/example/Projecte3MushTool/BusquedaActivity.kt
@@ -1,38 +1,23 @@
 package com.example.Projecte3MushTool
 
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.compose.runtime.remember
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,12 +25,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.Projecte3MushTool.CrearSetaActivity
+import com.example.Projecte3MushTool.EditarSetaActivity
+import com.example.Projecte3MushTool.MainActivity
+import com.example.Projecte3MushTool.Seta
 import com.example.lemonade.ui.theme.AppTheme
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 
 class BusquedaActivity : ComponentActivity() {
     private lateinit var Boletreference: DatabaseReference
@@ -63,12 +48,12 @@ class BusquedaActivity : ComponentActivity() {
         }
     }
 
-    // Esta función crea una nueva seta en la base de datos
-
-
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun BusquedaApp(context: Context) {
-        val setasState = remember { mutableStateOf<List<Seta>>(emptyList()) }
+        var setasState by remember { mutableStateOf<List<Seta>>(emptyList()) }
+        var selectedSeta by remember { mutableStateOf<Seta?>(null) }
+        var showDialog by remember { mutableStateOf(false) }
 
         LaunchedEffect(true) {
             val setas = mutableListOf<Seta>()
@@ -88,7 +73,7 @@ class BusquedaActivity : ComponentActivity() {
                             newSetas.add(seta)
                         }
                     }
-                    setasState.value = newSetas
+                    setasState = newSetas
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
@@ -99,52 +84,123 @@ class BusquedaActivity : ComponentActivity() {
 
         Scaffold(
             topBar = {
-                @OptIn(ExperimentalMaterial3Api::class)
+                // Define tu TopBar aquí
                 TopAppBar(
                     modifier = Modifier.background(Color(0xFF6B0C0C)),
-                    title = { },
+                    title = { }, // No se muestra texto en el título de la TopAppBar
                     actions = {
                         Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(Color(0xFF6B0C0C)),
-                            horizontalArrangement = Arrangement.SpaceBetween
+                            modifier = Modifier.fillMaxWidth().background(Color(0xFF6B0C0C)),
+                            horizontalArrangement = Arrangement.SpaceBetween // Distribuye los elementos de manera uniforme en la fila
                         ) {
-                            Text(
-                                "Mushtool",
-                                modifier = Modifier
-                                    .padding(10.dp)
-                                    .align(Alignment.CenterVertically),
-                                color = Color.White
-                            )
+                            Text("Mushtool", modifier = Modifier.padding(10.dp).align(Alignment.CenterVertically), color = Color.White) // Texto que se muestra en la esquina izquierda // Texto que se muestra en la esquina izquierda
 
                             Button(
                                 onClick = {
                                     val intent = Intent(context, MainActivity::class.java)
-                                    context.startActivity(intent) },
+                                    context.startActivity(intent)
+                                },
                                 modifier = Modifier
+                                    // Tamaño del botón
                                     .align(Alignment.CenterVertically)
                                     .background(Color(0xFF6B0C0C))
                             ) {
                                 Image(
-                                    painter = painterResource(R.drawable.boton_exit),
-                                    contentDescription = "Exit Button",
-                                    modifier = Modifier.size(30.dp, 30.dp)
+                                    painter = painterResource(R.drawable.boton_exit), // Cambiar con tu recurso
+                                    contentDescription = "Descripción de la imagen",
+                                    modifier = Modifier
+                                        .size(30.dp, 30.dp) // Tamaño de la imagen
+                                    // Hace que la imagen llene todo el espacio disponible del botón
                                 )
                             }
                         }
                     }
                 )
             },
+
         ) { innerPadding ->
             Box(modifier = Modifier.padding(innerPadding)) {
                 Column(modifier = Modifier.padding(16.dp)) {
+                    Button(
+                        onClick = {
+                            val intent = Intent(context, CrearSetaActivity::class.java)
+                            context.startActivity(intent)
+                        },
+                        modifier = Modifier
+                            .background(Color(0xFF6B0C0C))
+                    ) {
+                        Text("Añadir seta")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
                     LazyColumn {
-                        items(setasState.value) { seta ->
-                            Text(text = seta.sci_name)
-                            Text(text = seta.name)
-                            Text(text = ""+seta.warn_level+"")
+                        items(setasState) { seta ->
+                            Text(text = "Nombre cientifico: " + seta.sci_name)
+                            Text(text = "Nombre comun: " + seta.name)
+                            Text(text = "Nivel de peligrosidad (0-10): " + seta.warn_level + "")
+                            Row {
+                                Button(onClick = {
+                                    selectedSeta = seta
+                                    showDialog = true
+                                }) {
+                                    Text("Delete")
+                                }
+                                Button(onClick = {
+                                    val intent = Intent(context, EditarSetaActivity::class.java)
+                                    intent.putExtra("img_path", seta.img_path)
+                                    intent.putExtra("name", seta.name)
+                                    intent.putExtra("sci_name", seta.sci_name)
+                                    intent.putExtra("warn_level", seta.warn_level)
+                                    context.startActivity(intent)
+                                }) {
+                                    Text("Edit")
+                                }
+                            }
                             Spacer(modifier = Modifier.height(16.dp))
+
+                            if (showDialog) {
+                                AlertDialog(
+                                    onDismissRequest = {
+                                        showDialog = false
+                                        selectedSeta = null
+                                    },
+                                    title = {
+                                        Text("Confirmación")
+                                    },
+                                    text = {
+                                        Text("¿Seguro que quieres borrar esta seta?")
+                                    },
+                                    confirmButton = {
+                                        Button(
+                                            onClick = {
+                                                showDialog = false
+                                                selectedSeta?.let { seta ->
+                                                    // Remove seta from database
+                                                    Boletreference.child(seta.sci_name)
+                                                        .removeValue()
+                                                    Toast.makeText(
+                                                        context,
+                                                        "Seta eliminada correctamente",
+                                                        Toast.LENGTH_SHORT
+                                                    ).show()
+                                                    selectedSeta = null
+                                                }
+                                            }
+                                        ) {
+                                            Text("Confirmar")
+                                        }
+                                    },
+                                    dismissButton = {
+                                        Button(
+                                            onClick = {
+                                                showDialog = false
+                                                selectedSeta = null
+                                            }
+                                        ) {
+                                            Text("Cancelar")
+                                        }
+                                    }
+                                )
+                            }
                         }
                     }
                 }
---
+++ app/src/main/java/com/example/Projecte3MushTool/EditarSetaActivity.kt
@@ -0,0 +1,185 @@
+package com.example.Projecte3MushTool
+
+import androidx.compose.runtime.Composable
+
+
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lemonade.ui.theme.AppTheme
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class EditarSetaActivity : ComponentActivity() {
+    private lateinit var Boletreference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inicializar Firebase
+        Boletreference = FirebaseDatabase.getInstance().getReference("Bolet")
+
+        val imgPath = intent.getStringExtra("img_path")
+        val name = intent.getStringExtra("name")
+        val sciName = intent.getStringExtra("sci_name")
+        val warnLevel = intent.getIntExtra("warn_level", 0)
+
+        setContent {
+            AppTheme {
+                EditarSetaApp(this, imgPath, name, sciName, warnLevel)
+            }
+        }
+    }
+
+    @Composable
+    fun EditarSetaApp(context: Context, imgPath: String?, name: String?, sciName: String?, warnLevel: Int?) {
+        val imgPathState = remember { mutableStateOf(imgPath ?: "") }
+        val nameState = remember { mutableStateOf(name ?: "") }
+        val sciNameState = remember { mutableStateOf(sciName ?: "") }
+        val warnLevelState = remember { mutableStateOf(warnLevel?.toString() ?: "") }
+
+
+        Scaffold(
+            topBar = {
+                @OptIn(ExperimentalMaterial3Api::class)
+                TopAppBar(
+                    modifier = Modifier.background(Color(0xFF6B0C0C)),
+                    title = { },
+                    actions = {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color(0xFF6B0C0C)),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                "Mushtool",
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .align(Alignment.CenterVertically),
+                                color = Color.White
+                            )
+
+                            Button(
+                                onClick = {
+                                    val intent = Intent(context, MainActivity::class.java)
+                                    context.startActivity(intent) },
+                                modifier = Modifier
+                                    .align(Alignment.CenterVertically)
+                                    .background(Color(0xFF6B0C0C))
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.boton_exit),
+                                    contentDescription = "Exit Button",
+                                    modifier = Modifier.size(30.dp, 30.dp)
+                                )
+                            }
+                        }
+                    }
+                )
+            },
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    OutlinedTextField(
+                        value = imgPathState.value,
+                        onValueChange = { imgPathState.value = it },
+                        label = { Text("Ruta de la imagen") }
+                    )
+                    OutlinedTextField(
+                        value = nameState.value,
+                        onValueChange = { nameState.value = it },
+                        label = { Text("Nombre común") }
+                    )
+                    OutlinedTextField(
+                        value = sciNameState.value,
+                        onValueChange = { sciNameState.value = it },
+                        label = { Text("Nombre científico") }
+                    )
+                    OutlinedTextField(
+                        value = warnLevelState.value,
+                        onValueChange = { warnLevelState.value = it },
+                        label = { Text("Nivel de advertencia") }
+                    )
+                    Button(
+                        onClick = {
+                            val imgPath = imgPathState.value
+                            val name = nameState.value
+                            val sciName = sciNameState.value
+                            val warnLevel = warnLevelState.value.toIntOrNull() ?: 0
+
+                            // Obtener el ID de la seta
+
+                            // Verificar que todos los campos requeridos estén llenos
+                            if (imgPath.isNotEmpty() && name.isNotEmpty() && sciName.isNotEmpty()) {
+                                val setaReference = Boletreference.child(sciName)
+
+                                // Actualizar la seta en la base de datos
+                                setaReference.child("img_path").setValue(imgPath)
+                                setaReference.child("name").setValue(name)
+                                setaReference.child("sci_name").setValue(sciName)
+                                setaReference.child("warn_level").setValue(warnLevel)
+
+                                // Mensaje de éxito
+                                Toast.makeText(
+                                    context,
+                                    "Seta actualizada correctamente",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val intent = Intent(context, BusquedaActivity::class.java)
+                                context.startActivity(intent)
+                            } else {
+                                // Mensaje de error si algún campo está vacío
+                                Toast.makeText(
+                                    context,
+                                    "Todos los campos son obligatorios",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        }
+                    ) {
+                        Text("Actualizar seta")
+                    }
+                }
+            }
+
+        }
+    }
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppTheme {
+        LemonadeApp(LocalContext.current)
+        }
+    }
+}
