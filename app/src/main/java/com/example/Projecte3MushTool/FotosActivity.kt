ae439fb866c67943637796bb0e05d81ac199ad02
Pol González Miranda <pgmxx04@gmail.com>
2024-02-08 20:10:38 +0100

    corrección errores

--- app/src/main/java/com/example/Projecte3MushTool/FotosActivity.kt
+++ app/src/main/java/com/example/Projecte3MushTool/FotosActivity.kt
@@ -1,8 +1,16 @@
 package com.example.Projecte3MushTool
+
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -34,13 +42,26 @@ import java.util.concurrent.Executors
 
 class FotosActivity : ComponentActivity() {
 
-    private lateinit var cameraExecutor: ExecutorService
+    lateinit var imagen: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppTheme {
-                FotosApp(this)
-            }
+        setContentView(R.layout.fotos_activity)
+        val captura: Button = findViewById(R.id.btnFoto)
+        imagen = findViewById(R.id.imagenPhoto)
+        captura.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    val getAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val bitmap = it.data?.extras?.get("data") as Bitmap
+        imagen.setImageBitmap(bitmap)
+    }
+
+    fun takePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            getAction.launch(intent)
         }
     }
 
