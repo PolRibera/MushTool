85ad036f2fa0d774ba2df6c5f4393813298f4be8
polri <ribera.aymerich.pol@alumnat.copernic.cat>
2024-02-13 18:14:26 +0100

    Fotos

--- app/src/main/java/com/example/Projecte3MushTool/FotosActivity.kt
+++ app/src/main/java/com/example/Projecte3MushTool/FotosActivity.kt
@@ -2,73 +2,132 @@ package com.example.Projecte3MushTool
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.lemonade.ui.theme.AppTheme
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-
+import coil.compose.rememberImagePainter
 
 class FotosActivity : ComponentActivity() {
+    private val context = this
 
-    lateinit var imagen: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fotos_activity)
-        val captura: Button = findViewById(R.id.btnFoto)
-        imagen = findViewById(R.id.imagenPhoto)
-        captura.setOnClickListener {
-            takePhoto()
-        }
-        val volver: Button = findViewById(R.id.btnVolver)
-        volver.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+        setContent {
+            FotosApp(context)
         }
     }
 
-    val getAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+    private val getAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         val bitmap = it.data?.extras?.get("data") as Bitmap
-        imagen.setImageBitmap(bitmap)
+        capturedBitmap = bitmap
     }
 
-    fun takePhoto() {
+    var capturedBitmap by mutableStateOf<Bitmap?>(null)
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun FotosApp(context: ComponentActivity) {
+        Scaffold(
+            topBar = {
+                @OptIn(ExperimentalMaterial3Api::class)
+                TopAppBar(
+                    modifier = Modifier.background(Color(0xFF6B0C0C)),
+                    title = { },
+                    actions = {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color(0xFF6B0C0C)),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                "Mushtool",
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .align(Alignment.CenterVertically),
+                                color = Color.White
+                            )
+
+                            Button(
+                                onClick = {
+                                    val intent = Intent(context, MainActivity::class.java)
+                                    context.startActivity(intent)
+                                },
+                                modifier = Modifier
+                                    .align(Alignment.CenterVertically)
+                                    .background(Color(0xFF6B0C0C))
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.boton_exit),
+                                    contentDescription = "Exit Button",
+                                    modifier = Modifier.size(30.dp, 30.dp)
+                                )
+                            }
+                        }
+                    }
+                )
+            },
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // Usamos la función `remember` para asegurarnos de que la imagen se actualice correctamente
+                    val bitmap by remember { mutableStateOf(capturedBitmap) }
+
+                    bitmap?.let { capturedBitmap ->
+                        Image(
+                            painter = rememberImagePainter(capturedBitmap),
+                            contentDescription = "Captured Image",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(300.dp)
+                        )
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Button(
+                            onClick = { takePhoto(context) },
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        ) {
+                            Text(text = "Tomar Foto")
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    fun takePhoto(context: Context) {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (intent.resolveActivity(packageManager) != null) {
+        if (intent.resolveActivity(context.packageManager) != null) {
             getAction.launch(intent)
         }
     }
 }
-
