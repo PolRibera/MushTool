e7197fb8ea263b9a9fa35c8290543a97979ab0d5
Pol González Miranda <pgmxx04@gmail.com>
2024-03-11 18:14:28 +0100

    cambiada view editar foro post

--- app/src/main/java/com/example/Projecte3MushTool/EditForoPostActivity.kt
+++ app/src/main/java/com/example/Projecte3MushTool/EditForoPostActivity.kt
@@ -1,64 +1,141 @@
 package com.example.Projecte3MushTool
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lemonade.ui.theme.AppTheme
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
 
-class EditForoPostActivity : AppCompatActivity() {
+class EditForoPostActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
-    private lateinit var database: DatabaseReference
     private lateinit var postId: String
-    private lateinit var editTextPost: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_foro_post)
+        auth = FirebaseAuth.getInstance()
 
-        auth = Firebase.auth
-        database = FirebaseDatabase.getInstance().reference.child("posts")
-
-        editTextPost = findViewById(R.id.editTextPost)
+        val editTextPostState = mutableStateOf("")
 
         postId = intent.getStringExtra("postId") ?: ""
 
-        // Obtener el texto actual del post desde Firebase y mostrarlo en el EditText
-        database.child(postId).get().addOnSuccessListener { snapshot ->
-            val post = snapshot.getValue(ForoPost::class.java)
-            editTextPost.setText(post?.text)
-        }.addOnFailureListener { e ->
-            Log.e("EditForoPostActivity", "Error al obtener el post: $e")
-        }
-
-        val buttonSave = findViewById<Button>(R.id.buttonSave)
-        buttonSave.setOnClickListener {
-            // Guardar el texto editado del post en Firebase
-            val newText = editTextPost.text.toString().trim()
-            if (newText.isNotEmpty()) {
-                val userId = auth.currentUser?.uid
-                if (userId != null) {
-                    database.child(postId).child("text").setValue(newText)
-                        .addOnSuccessListener {
-                            Toast.makeText(this@EditForoPostActivity, "Post editado exitosamente", Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                        .addOnFailureListener { e ->
-                            Log.e("EditForoPostActivity", "Error al editar el post: $e")
-                        }
-                } else {
-                    Toast.makeText(this@EditForoPostActivity, "Usuario no autenticado", Toast.LENGTH_SHORT).show()
-                    finish()
-                }
-            } else {
-                Toast.makeText(this@EditForoPostActivity, "Debes ingresar un texto", Toast.LENGTH_SHORT).show()
+        setContent {
+            AppTheme {
+                EditForoPostActivityView(this, auth, postId, editTextPostState)
             }
         }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun EditForoPostActivityView(
+        context: Context,
+        auth: FirebaseAuth,
+        postId: String,
+        editTextPost: MutableState<String>
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier.background(Color(0xFF6B0C0C)),
+                    title = { }, // No se muestra texto en el título de la TopAppBar
+                    navigationIcon = {
+                        IconButton(onClick = { onBackPressed() }) {
+                            Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                        }
+                    },
+                    actions = {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color(0xFF6B0C0C)),
+                            horizontalArrangement = Arrangement.SpaceBetween // Distribuye los elementos de manera uniforme en la fila
+                        ) {
+                            Text(
+                                "Editar Post",
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .align(Alignment.CenterVertically),
+                                color = Color.White
+                            )
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = editTextPost.value,
+                        onValueChange = { editTextPost.value = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text("Escribe tu post aquí") }
+                    )
+                    Button(
+                        onClick = {
+                            // Guardar el texto editado del post en Firebase
+                            val newText = editTextPost.value.trim()
+                            if (newText.isNotEmpty()) {
+                                val userId = auth.currentUser?.uid
+                                if (userId != null) {
+                                    val database = FirebaseDatabase.getInstance().reference.child("ForoPost")
+                                    database.child(postId).child("text").setValue(newText)
+                                        .addOnSuccessListener {
+                                            Toast.makeText(
+                                                context,
+                                                "Post editado exitosamente",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                            // Si deseas cerrar la actividad después de editar el post, descomenta la siguiente línea
+                                            // (context as EditForoPostActivity).finish()
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Log.e("EditForoPostActivity", "Error al editar el post: $e")
+                                        }
+                                } else {
+                                    Toast.makeText(context, "Usuario no autenticado", Toast.LENGTH_SHORT).show()
+                                }
+                            } else {
+                                Toast.makeText(context, "Debes ingresar un texto", Toast.LENGTH_SHORT).show()
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(top = 16.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Guardar")
+                    }
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        EditForoPostActivity()
     }
 }
